060ae8af6bb621b217fed6b362738ac24ced2990
ming wu <a85062@pchome.com.tw>
2021-06-19 18:46:41 +0800

    build

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/vue-week6-cli/dist/' : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? '/vue-week6/dist/' : '/',
 };
